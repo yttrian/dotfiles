8cd1b8edae13517ce1a0e142d7ca4c8d649034c3
Ian Moore <privet.privet250@gmail.com>
2021-01-01 18:56:31 -0500

    Adjust fisher and server install goals

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    curl https://git.io/fisher | source
+    fisher
 end
 
 set theme_title_display_user yes
